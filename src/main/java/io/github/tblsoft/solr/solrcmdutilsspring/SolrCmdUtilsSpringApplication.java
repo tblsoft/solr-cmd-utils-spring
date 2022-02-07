4917e4903cb60513e5499d4822d23f714fa10449
Thomas Bärwinkel <tbl@gmx.de>
2022-02-07 23:32:03 +0100

    initial import

---
+++ src/main/java/io/github/tblsoft/solr/solrcmdutilsspring/SolrCmdUtilsSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.tblsoft.solr.solrcmdutilsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrCmdUtilsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrCmdUtilsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tblsoft/solr/solrcmdutilsspring/SolrCmdUtilsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tblsoft.solr.solrcmdutilsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrCmdUtilsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
